27a66547f8d131d9c66a5d6fac40c6fe10c066bd
JiaVandelli <jiazhuang.vandelli@gmail.com>
2026-05-21 21:27:45 +0200

    Create zone.js

---
+++ zone.js
@@ -0,0 +1,7 @@
+// zone.js
+export const destinatari = {
+  "LI P244": { A: "protocollo.prefrm@pec.interno.it", Cc: "protocollo@pec.enac.gov.it; mobilita.innovativa@enac.gov.it" },
+  "R1": { A: "operazioni@enav.it", Cc: "mobilita.innovativa@enac.gov.it" },
+  "P123": { A: "enac@pec.enac.gov.it", Cc: "" },
+  // Aggiungi altre zone qui...
+};
